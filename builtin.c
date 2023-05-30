e087a1986040ecda09a84269400b099ce531d83e
mynamedust <mynamedust@gmail.com>
2023-05-30 22:24:31 +0400

    add pwd

--- builtin.c
+++ builtin.c
@@ -1,14 +1,26 @@
 #include <unistd.h>
+#include <limits.h>
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	if (!s || fd < 0)
-		return ;
-	while (*s)
-	{
-		write(fd, s, 1);
-		s++;
-	}
+    if (!s || fd < 0)
+        return ;
+    while (*s)
+    {
+        write(fd, s, 1);
+        s++;
+    }
+}
+
+void    ft_pwd()
+{
+    char    path[PATH_MAX];
+
+    if (getcwd(path, PATH_MAX))
+    {
+        ft_putstr_fd(path, 1);
+        write(1, "\n", 1);
+    }
 }
 
 void	ft_echo(int fd, char **args, int count, int n)
@@ -16,7 +28,7 @@ void	ft_echo(int fd, char **args, int count, int n)
 	int	i;
 
 	i = 0;
-	while (i < n)
+	while (i < count)
 	{
 		ft_putstr_fd(args[i], fd);
 		i++;
